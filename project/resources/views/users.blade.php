23892542f6fcfe2c450da60c8e1860635e60dff4
Henrique3040 <henrique.de.sousa.lima.cardoso@student.ehb.be>
2023-12-29 14:30:29 +0100

    Feat: Admin
    admins kan nu andere admins aanmaken
    
    reviewd-by: Henrique
    version#3

--- project/resources/views/users.blade.php
+++ project/resources/views/users.blade.php
@@ -9,6 +9,9 @@
 <body>
     
     <table border="1">
+        <tr>
+            <th colspan="4">Users</th>
+        </tr>
     <tr>
         <th>ID</th>
         <th>Name</th>
@@ -36,5 +39,59 @@
 
     @endforeach
     </table>
+
+
+    <table border="1">
+        <tr>
+            <th colspan="4">Admins</th>
+        </tr>
+        <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Email</th>
+        </tr>
+    
+        @foreach($users as $user)
+    
+        @if($user->admin)
+        <tr>
+            <td>{{ $user->id }}</td>
+            <td>{{ $user->name }}</td>
+            <td> {{ $user->email }}</td>
+        </tr>
+        @endif
+     
+    
+        @endforeach
+        </table>
+    
+
+<div>
+    <form method="post" action="{{ route('register.admin') }}" enctype="multipart/form-data">
+        @csrf
+        @method('post')
+      <div>
+           <input type="text" name="name" placeholder="name">
+      </div>
+
+        <div>
+        <input type="email" name="email" placeholder="email">
+        </div>
+
+        <div>
+        <input type="password" name="password" placeholder="password">
+        </div>
+
+        <div>
+        <input type="password" name="password_confirmation" placeholder="password_confirmation">
+        </div>
+
+        <div>
+        <button type="submit">Register</button>
+        </div>
+    </form>
+
+</div>
+
 </body>
 </html>
