162d535b67c2be10649a233676e4bd056b80bf18
Henrique3040 <henrique.de.sousa.lima.cardoso@student.ehb.be>
2024-01-03 16:40:37 +0100

    feat: desing
    het desing van create post, edit post, about page en user list zijn af
    
    reviewd-by: Henrique
    version#3

--- project/resources/views/about.blade.php
+++ project/resources/views/about.blade.php
@@ -2,41 +2,42 @@
 
 @section('content')
 
-<div class="container">
-    <h1>Introduction</h1>
-    <p>This website is the result of the work of one student from Erasmus Brussels University College. This website is the result of my Backend web Laravel project, aiming to make a personal sort of social media.</p>
+<div class="container-about">
 
-    <h2>Features</h2>
-    <h3>Making posts</h3>
-    <ul>
+    <h1 class="about-heading">Introduction</h1>
+    <p class="about-paragraph">This website is the result of the work of one student from Erasmus Brussels University College. This website is the result of my Backend web Laravel project, aiming to make a personal sort of social media.</p>
+
+    <h2 class="about-subheading">Features</h2>
+    <h3 class="about-subsubheading">Making posts</h3>
+    <ul class="about-list">
         <li>Make an account to be able to make posts with titles and a caption</li>
         <li>Give posts a like</li>
     </ul>
 
-    <h3>Profile</h3>
-    <ul>
+    <h3 class="about-subsubheading">Profile</h3>
+    <ul class="about-list">
         <li>View and update your profile information</li>
         <li>Delete your account from the database</li>
     </ul>
 
-    <h3>Contact</h3>
-    <ul>
+    <h3 class="about-subsubheading">Contact</h3>
+    <ul class="about-list">
         <li>Users can send a note to admins from the website</li>
     </ul>
 
-    <h2>Technology Stack</h2>
-    <ul>
+    <h2 class="about-subheading">Technology Stack</h2>
+    <ul class="about-list">
         <li>Database: MySQL</li>
         <li>Framework: Laravel</li>
     </ul>
 
-    <h2>Links</h2>
-    <p>Here are the links that were used to help making this webSite</p>
-    <ul>
+    <h2 class="about-subheading">Links</h2>
+    <p class="about-paragraph">Here are the links that were used to help in making this website</p>
+    <ul class="about-link-list">
         <li><a href="https://getbootstrap.com/">Bootstrap</a></li>
-        <li><a href="https://laravel.com/docs/10.x">Lravel documetation</a></li>
-        <li><a href="https://www.youtube.com/watch?v=_LA9QsgJ0bw">Laravel tutorial</a></li>
-        <li><a href="https://github.com/Henrique3040/WebSite">Git Hub</a></li>
+        <li><a href="https://laravel.com/docs/10.x">Laravel Documentation</a></li>
+        <li><a href="https://www.youtube.com/watch?v=_LA9QsgJ0bw">Laravel Tutorial</a></li>
+        <li><a href="https://github.com/Henrique3040/WebSite">GitHub Repository</a></li>
     </ul>
 
 </div>
