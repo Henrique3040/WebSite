ce4ef02beaab8f5357db59929aaf14ad759790d6
Henrique3040 <henrique.de.sousa.lima.cardoso@student.ehb.be>
2024-01-07 20:15:19 +0100

    Fix: About
    about page uitgebereid, met likns
    
    reviewd-by: Henrique
    version#2

--- project/resources/views/about.blade.php
+++ project/resources/views/about.blade.php
@@ -14,12 +14,14 @@
     <ul class="about-list">
         <li>Make an account to be able to make posts with titles and a caption</li>
         <li>Give posts a like</li>
+        <li>Visit others user profile page</li>
     </ul>
 
     <h3 class="about-subsubheading">Profile</h3>
     <ul class="about-list">
         <li>View and update your profile information</li>
         <li>Delete your account from the database</li>
+        <li>Change you password if needed</li>
     </ul>
 
     <h3 class="about-subsubheading">Contact</h3>
@@ -36,10 +38,40 @@
     <h2 class="about-subheading">Links</h2>
     <p class="about-paragraph">Here are the links that were used to help in making this website</p>
     <ul class="about-link-list">
-        <li><a href="https://getbootstrap.com/">Bootstrap</a></li>
-        <li><a href="https://laravel.com/docs/10.x">Laravel Documentation</a></li>
-        <li><a href="https://www.youtube.com/watch?v=_LA9QsgJ0bw">Laravel Tutorial</a></li>
-        <li><a href="https://github.com/Henrique3040/WebSite">GitHub Repository</a></li>
+        <li>
+            <a href="https://getbootstrap.com/">Bootstrap</a>
+            <p>I have use bootstrap for inspiration for desing</p>
+        </li>
+
+        <li>
+            <a href="https://laravel.com/docs/10.x">Laravel Documentation</a>
+            <p>The laravel documation was very usefull for the coding part and for the use from the data base</p>
+        </li>
+
+        <li>
+            <a href="https://www.youtube.com/watch?v=_LA9QsgJ0bw">Laravel Tutorial</a>
+            <p>This tutorial next to the documation have also be usefull, I did use for beter understanding from how laravel works</p>
+        </li>
+
+        <li>
+            <a href="https://github.com/Henrique3040/WebSite">GitHub Repository</a>
+            <p>This is the link to may git, whre you can see the hole code</p>
+        </li>
+
+            <li>
+                <a href="https://chat.openai.com/">ChatGPT</a>
+                <p>ChatGPT was very usefull the help find code errors and understand errors messages also did help a lot with the desing part since I'm not good at all</p>
+            </li>
+
+            <li>
+                <a href="https://ehb.instructuremedia.com/embed/edeb8a9e-3e8f-4af3-85f9-8be5ae754e7d">Tutorial Guestbook</a>
+                <p>I used this tutorial to have some ispirations and also did help with beter understanding from migrations and the use of models and controllers</p>
+            </li>
+
+            <li>
+                <a href="https://www.w3schools.com/">W3Schools</a>
+                <p>I did use W3Schools also for hulp with the desing part</p>
+            </li>
     </ul>
 
 </div>
